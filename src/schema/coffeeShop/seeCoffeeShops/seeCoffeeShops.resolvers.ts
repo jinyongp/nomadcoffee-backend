10e3b0864637d9e492fac8935cc2b6e65cda1ef1
jinyongp <dev.jinyongp@gmail.com>
2022-02-10 01:19:54 +0900

    Enable seeCoffeeShop to filter by userId

--- src/schema/coffeeShop/seeCoffeeShops/seeCoffeeShops.resolvers.ts
+++ src/schema/coffeeShop/seeCoffeeShops/seeCoffeeShops.resolvers.ts
@@ -1,11 +1,14 @@
 import { CursorArgs, Resolvers } from '@types';
 
+type SeeCoffeeShopsArgs = CursorArgs & { userId?: number };
+
 export default {
   Query: {
-    seeCoffeeShops: async (_, { items, lastId }: CursorArgs, { client }) => {
+    seeCoffeeShops: async (_, { items, lastId, userId }: SeeCoffeeShopsArgs, { client }) => {
       const coffeeShops = await client.coffeeShop.findMany({
         take: items,
         ...(lastId && { skip: 1, cursor: { id: lastId } }),
+        ...(userId && { where: { userId } }),
       });
       return { ok: true, coffeeShops };
     },
